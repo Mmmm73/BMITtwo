67214b43c5f5d42fc41f6a4add250c5869825da4
melvinmelvin1922@gmail.com <Passwordistoostrong>
2023-07-14 09:12:25 +0300

    changed port

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const app = express();
-const port = 8080;
+//const port = 8080;
+const port = process.env.PORT || 3000;
+
 app.use(express.urlencoded());
 
 app.set('views', path.join(__dirname)+"/views");
@@ -49,4 +51,5 @@ app.post('/records', jsonParser, (request, response) => {
 
 app.listen(port);
 console.log('server is listening to port 3300');
+console.log(`server listening on port ${port}`);
 
