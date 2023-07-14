49167b4c54bb713dadd1c55715126cc446295bf8
melvinmelvin1922@gmail.com <Passwordistoostrong>
2023-07-14 10:52:32 +0300

    changed port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const app = express();
-const port = 8080;
-//const port = process.env.PORT || 8080;
+//const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded());
 
